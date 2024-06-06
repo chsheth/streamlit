e4708fd3fec3d012bba9d904b9f6e2357797f76b
Chintan Sheth <chsheth@gmail.com>
2024-06-05 21:34:09 -0400

    completed code

--- mushrooms/app.py
+++ mushrooms/app.py
@@ -86,28 +86,48 @@ def main():
             st.write("Recall", recall_score(y_test, y_pred, labels=class_names).round(2))
             plot_metrics(metrics, y_test, y_pred)
 
-    # if classifier == 'Logistic Regression':
-    #     st.sidebar.subheader("Select Hyperparameters")
-    #     C = st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, step=0.01, key='C_LR')
-    #     kernel = st.sidebar.radio("Kernel", ('rbf', 'linear'), key="kernel")
-    #     gamma = st.sidebar.radio("Gamma (Kernel Coeff)", ("scale", "auto"), key="gamma" )
+    if classifier == 'Logistic Regression':
+        st.sidebar.subheader("Select Hyperparameters")
+        C = st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, step=0.01, key='C_LR')
+        max_iter = st.sidebar.slider("Maximum number of iterations", 100, 500, key="max_iter")
 
-    #     metrics = st.sidebar.multiselect("Select metrics to plot", ("Confusion Matrix", "ROC Curve", "Precision-Recall Curve"))
+        metrics = st.sidebar.multiselect("Select metrics to plot", ("Confusion Matrix", "ROC Curve", "Precision-Recall Curve"))
 
-    #     if st.sidebar.button("Classify", key="classify"):
-    #         st.subheader("SVM Results")
+        if st.sidebar.button("Classify", key="classify"):
+            st.subheader("Logistic Regression Results")
             
-    #         model = SVC(C=C, kernel=kernel, gamma=gamma)
-    #         model.fit(x_train, y_train)
+            model = LogisticRegression(C=C, penalty='l2', max_iter=max_iter)
+            model.fit(x_train, y_train)
 
-    #         y_pred = model.predict(x_test)
-    #         accuracy = accuracy_score(y_test, y_pred)
+            y_pred = model.predict(x_test)
+            accuracy = accuracy_score(y_test, y_pred)
 
-    #         st.write("Accuracy", accuracy)
-    #         st.write("Precision", precision_score(y_test, y_pred, labels=class_names).round(2))
-    #         st.write("Recall", recall_score(y_test, y_pred, labels=class_names).round(2))
-    #         plot_metrics(metrics, y_test, y_pred)
+            st.write("Accuracy", accuracy)
+            st.write("Precision", precision_score(y_test, y_pred, labels=class_names).round(2))
+            st.write("Recall", recall_score(y_test, y_pred, labels=class_names).round(2))
+            plot_metrics(metrics, y_test, y_pred)
 
+    if classifier == 'Random Forest':
+        st.sidebar.subheader("Select Hyperparameters")
+        n_estimators = st.sidebar.number_input("The number of trees in the forest", 100, 5000, step=10, key='n_estimators')
+        max_depth = st.sidebar.number_input("The maximum depth of the tree", 1, 20, step=1, key="n_estimators")
+        bootstrap = st.sidebar.radio('Bootstrap samples when building trees', ('True', 'False'), keys='bootstrap' )
+
+        metrics = st.sidebar.multiselect("Select metrics to plot", ("Confusion Matrix", "ROC Curve", "Precision-Recall Curve"))
+
+        if st.sidebar.button("Classify", key="classify"):
+            st.subheader("Random Forest Results")
+            
+            model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, bootstrap=bootstrap, n_jobs=-1)
+            model.fit(x_train, y_train)
+
+            y_pred = model.predict(x_test)
+            accuracy = accuracy_score(y_test, y_pred)
+
+            st.write("Accuracy", accuracy)
+            st.write("Precision", precision_score(y_test, y_pred, labels=class_names).round(2))
+            st.write("Recall", recall_score(y_test, y_pred, labels=class_names).round(2))
+            plot_metrics(metrics, y_test, y_pred)
 
 
 
